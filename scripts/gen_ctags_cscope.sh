ec8174636037c0b814faaf78e53ebf8337f5c4a3
hanyuhang <18700873675@163.com>
2025-07-04 23:23:12 +0800

    update scripts/gen_ctags_cscope.sh

--- scripts/gen_ctags_cscope.sh
+++ scripts/gen_ctags_cscope.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #ctags -R *
-ctags -R --c++-kinds=+p --fields=+iaS --extra=+q  ./
+ctags -R --c++-kinds=+p --fields=+iaS --extras=+q  ./
 
 
 find . -regex '.*\.c\|.*\.h\|.*\.cpp\|.*\.hpp\|.*\.java' > cscope.files
