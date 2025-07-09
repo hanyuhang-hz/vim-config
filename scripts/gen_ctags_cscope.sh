9a1e874762775685b68e70f513486cffd7e708fc
hanyuhang <18700873675@163.com>
2025-07-09 21:49:38 +0800

    add *.cc

--- scripts/gen_ctags_cscope.sh
+++ scripts/gen_ctags_cscope.sh
@@ -4,7 +4,7 @@
 ctags -R --c++-kinds=+p --fields=+iaS --extras=+q  ./
 
 
-find . -regex '.*\.c\|.*\.h\|.*\.cpp\|.*\.hpp\|.*\.java' > cscope.files
+find . -regex '.*\.c\|.*\.h\|.*\.cpp\|.*\.cc\|.*\.hpp\|.*\.java' > cscope.files
 cscope -Rbq
 
 
